d25dc93ec40eab7f5ca3c831284f27db554b1b07
ireneuszcierpisz <>
2024-01-24 23:10:14 +0000

    Write the POST handler of a collaboration request and save the submitted data.

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
@@ -10,8 +11,17 @@ def about_me(request):
     # queryset = About.objects.all().order_by('-updated_on').first()
     # about = get_object_or_404(queryset)
     about = About.objects.all().order_by('-updated_on').first()
+    
+    if request.method == "POST":
+        print("Received a POST request from collaborate form.")
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid:
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
     collaborate_form = CollaborateForm()
-
     return render(
         request,
         "about/about.html",
