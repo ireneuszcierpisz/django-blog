df61362db4813a3974a0b12749885d01d5616b84
ireneuszcierpisz <>
2024-01-28 10:22:00 +0000

    Add django tests

--- codestar/settings.py
+++ codestar/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import sys
 from django.contrib.messages import constants as messages
 import dj_database_url
 if os.path.isfile('env.py'):
@@ -110,7 +111,10 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # }
 
 DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
+    
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
     "https://*.herokuapp.com"
